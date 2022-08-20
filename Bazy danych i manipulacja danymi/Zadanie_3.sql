29f7b9946eb0a20d4408038e137528b27b4fa988
Edyta Stodołkiewicz <stodolkiewicz.edyta@gmail.com>
2022-08-20 14:46:01 +0000

    Add new file

---
+++ Bazy danych i manipulacja danymi/Zadanie_3.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE Pracownicy;
+USE Pracownicy;
+CREATE TABLE Pracownicy (
+ID INTEGER UNIQUE PRIMARY KEY,
+Imię TEXT DEFAULT NULL,
+Nazwisko TEXT DEFAULT NULL,
+Wiek INTEGER DEFAULT NULL,
+Kurs TEXT DEFAULT NULL
+);
+SELECT * FROM Pracownicy;
+UPDATE Pracownicy
+SET Imię = NULL WHERE Imię = '';
+UPDATE Pracownicy
+SET Nazwisko = NULL WHERE Nazwisko = '';
+UPDATE Pracownicy
+SET Wiek = NULL WHERE Wiek = '';
+UPDATE Pracownicy
+SET Kurs = NULL WHERE Kurs = '';
+SELECT * FROM Pracownicy;
+SELECT * FROM Pracownicy WHERE Nazwisko = 'KOWALCZYK';
+SELECT * FROM Pracownicy WHERE Wiek >= 30 AND Wiek <= 40;
+SELECT * FROM Pracownicy WHERE Nazwisko NOT LIKE '%and%';
+SELECT * FROM Pracownicy WHERE ID >=1 AND ID <= 7;
+SELECT * FROM Pracownicy WHERE ID IS NULL OR Imię IS NULL OR Nazwisko IS NULL OR Wiek IS NULL OR Kurs IS NULL;
+SELECT * FROM Pracownicy WHERE Kurs IS NULL;
