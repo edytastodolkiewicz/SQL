6b1e2673dba744046d2a85317e50eab14ff9e28e
Edyta Stodołkiewicz <stodolkiewicz.edyta@gmail.com>
2022-08-21 17:28:00 +0000

    Add new file

---
+++ Podstawowa składnia języka SQL/Zadanie_3.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE aggregates;
+USE aggregates;
+CREATE TABLE aggregates.batman(
+  id INTEGER PRIMARY KEY NOT NULL,
+  first_name TEXT,
+  last_name TEXT,
+  sex CHAR(1),
+  age INTEGER,
+  price DOUBLE,
+  start_date DATE,
+  gift TEXT
+);
+INSERT INTO aggregates.batman VALUES (1, 'Alicja', 'Rogal', 'F', 16, 100.25,'2020-01-02', 'rower' );
+INSERT INTO aggregates.batman VALUES (2, 'Iwona', 'Kowalska', 'F', 33, 56.58, '2020-01-03', 'komputer');
+INSERT INTO aggregates.batman VALUES (3, 'Igor', 'Kowalski', 'M', 50, 68.00, '2020-01-04', 'karty');
+INSERT INTO aggregates.batman VALUES (4, 'Kamil', 'Juszczak', 'M', 50, 40.87, '2020-01-05', 'piłka'); 
+INSERT INTO aggregates.batman VALUES (5, 'Konrad', 'Kowal', 'M', 18, 32.63, '2020-01-06', 'herbata' );
+INSERT INTO aggregates.batman VALUES (6, 'Iwona', 'Feniks', 'F', 35, 78.98, '2020-01-07', 'okno' );
+INSERT INTO aggregates.batman VALUES (7, 'Robert', 'Lew', 'M', 40, 120.32, '2020-01-08', 'drzwi');
+INSERT INTO aggregates.batman VALUES (8, 'Tomasz', 'Nowak', 'M', 60, 150.00, '2020-01-09', 'korona');
+INSERT INTO aggregates.batman VALUES (9, 'Aldona', 'Buk', NULL, NULL, 121.25, '2020-01-10', 'wycieczka');
+SELECT * FROM batman;
+-- a)
+SELECT YEAR(start_date), MONTH(start_date), DAY(start_date) FROM batman;
+-- b)
+SELECT start_date, DATE_ADD(start_date, INTERVAL 3 DAY) AS final_date FROM batman;
+SELECT start_date, ADDDATE(start_date, 3) AS final_date FROM batman;
+-- c)
+SELECT CURRENT_DATE();
+SELECT CURDATE();
+-- d)
+SELECT MONTHNAME(CURDATE());
+-- e)
+SELECT start_date, WEEK(start_date), MONTHNAME(start_date), QUARTER(start_date), DAYOFYEAR(start_date) FROM batman;
