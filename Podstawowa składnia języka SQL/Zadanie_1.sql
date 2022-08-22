622c167d6abc8380b77902f76aa69ec4bea6ecbb
Edyta Stodołkiewicz <stodolkiewicz.edyta@gmail.com>
2022-08-22 12:05:05 +0000

    Update Podstawowa składnia języka SQL/Zadanie_1.sql

--- Podstawowa składnia języka SQL/Zadanie_1.sql
+++ Podstawowa składnia języka SQL/Zadanie_1.sql
@@ -41,6 +41,8 @@ SELECT max(price) FROM batman;
 SELECT max(start_date) FROM batman;
 -- i)
 SELECT max(last_name) FROM batman;
+SELECT last_name FROM batman LIMIT 1
+ORDER BY last_name DESC;
 -- j)
 SELECT min(price) FROM batman;
 -- k)
